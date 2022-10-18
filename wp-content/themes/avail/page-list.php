5cab8f08624bca7e21b2bdc73bcbf12fabf2c38d
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-10-18 16:34:28 -0300

    2022_10_18_16_34_26

---
+++ wp-content/themes/avail/page-list.php
@@ -0,0 +1,220 @@
+<?php get_template_part( 'header_system' ); ?>
+<?php get_template_part( 'menu_system' ); ?>
+
+      <div class="panel-header panel-header-sm">
+      </div>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title"><?php echo get_the_title(); ?></h4>
+              </div>
+
+
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>
+                        NOMBRE
+                      </th>
+                      <th>
+                        NEMOTÉCNICO
+                      </th>
+                      <th>
+                        TRADER
+                      </th>
+                      <th>
+                        NUM
+                      </th>
+                      <th>
+                        ESTADOS
+                      </th>
+
+                    </thead>
+
+                    <tbody>
+
+                      <tr>
+                        <td>
+                          BANCO DE CREDITO E INVERSIONES
+                        </td>
+                        <td>
+                          BCI
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          2
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=BCI">Descargar</a>
+                        </td>
+                      </tr>
+
+
+                      <tr>
+                        <td>
+                        SOC. INVER. PAMPA CALICHERA S.A. SERIE A
+                        </td>
+                        <td>
+                          CALICHERAA
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          9
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=CALICHERAA">Descargar</a>
+                        </td>
+                      </tr>
+
+                      <tr>
+                        <td>
+                          BANCO DE CHILE
+                        </td>
+                        <td>
+                          CHILE
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          957
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=CHILE">Descargar</a>
+                        </td>
+                      </tr>             
+
+                      <tr>
+                        <td>
+                          ENTEL CHILE
+                        </td>
+                        <td>
+                          ENELCHILE
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          861
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=ENELCHILE">Descargar</a>
+                        </td>
+                      </tr> 
+
+                      <tr>
+                        <td>
+                          NITRATOS DE CHILE S.A.
+                        </td>
+                        <td>
+                          NITRATOS
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          667
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=NITRATOS">Descargar</a>
+                        </td>
+                      </tr> 
+            
+                      <tr>
+                        <td>
+                          SOCIEDAD DE INVERSIONES ORO BLANCO S.A.
+                        </td>
+                        <td>
+                          ORO BLANCO
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          3032
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=ORO%20BLANCO">Descargar</a>
+                        </td>
+                      </tr>            
+
+                      <tr>
+                        <td>
+                          SOC QUIMICA MINERA DE CHILE S.A. SERIE A
+                        </td>
+                        <td>
+                          SQM-A
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          1
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=SQM-A">Descargar</a>
+                        </td>    
+                      </tr>        
+
+                      <tr>
+                        <td>
+                          SOC QUIMICA MINERA DE CHILE S.A. SERIE B
+                        </td>
+                        <td>
+                          SQM-B
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          1
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=SQM-B">Descargar</a>
+                        </td>  
+                      </tr>
+
+                      <tr>
+                        <td>
+                          COMPANIA SUD AMERICANA DE VAPORES S.A.
+                        </td>
+                        <td>
+                          VAPORES
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td class="text-right">
+                          1
+                        </td>
+                        <td class="text-center">
+                          <a href="http://apiws.bolsadesantiago.com/ifrs/newobtenerpdf.asp?nemo=VAPORES">Descargar</a>
+                        </td>
+                      </tr>
+
+
+
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+
+            </div>
+          </div>
+    
+        </div>
+
+        <div class="row">
+
+
+        </div>          
+    </div>
+
+ <?php get_template_part( 'footer_system' ); ?>
