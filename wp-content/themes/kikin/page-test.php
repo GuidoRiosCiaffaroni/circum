f3a13564dba8b61a539379a9565df11ee61b0261
guidoriosciaffaroni <guidoriosciaffaroni@gmail.com>
2022-11-02 23:17:27 -0300

    2022_11_02_23_17_25

---
+++ wp-content/themes/kikin/page-test.php
@@ -0,0 +1,288 @@
+<?php get_header(); ?>
+<?php get_template_part( 'system_menu' ); ?>
+
+      <div class="panel-header panel-header-sm">
+      </div>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title"><?php echo get_the_title(); ?></h4>
+              </div>
+
+
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>
+                        NOMBRE
+                      </th>
+                      <th>
+                        BSTGO
+                      </th>
+                      <th>
+                        YAHOO
+                      </th>
+                      <th>
+                        GOOGLE
+                      </th>
+                      <th>
+                        INVESTING
+                      </th>
+                      <th>
+                        CMF
+                      </th>
+                      <th>
+                        TRADER
+                      </th>
+                      <th>
+                        A
+                      </th>
+                      <th>
+                        B
+                      </th>
+                      <th>
+                        C
+                      </th>
+                      <th>
+                        D
+                      </th>
+                    </thead>
+
+                    <tbody>
+
+                      <!-- --------------------------------------------------------------------------------------------- -->
+                      <tr>
+                        <td>
+                          BANCO DE CREDITO E INVERSIONES
+                        </td>
+                        <td>
+                          <a href="https://gat.bolsadesantiago.com/GAT/chartIQ/GAT_Resumen_CIQ.asp?language=es&NEMO=BCI">
+                          BCI
+                          </a>
+                        </td>
+                        <td>
+                          <a href="https://finance.yahoo.com/chart/BCI.SN#eyJpbnRlcnZhbCI6IndlZWsiLCJwZXJpb2RpY2l0eSI6MSwidGltZVVuaXQiOm51bGwsImNhbmRsZVdpZHRoIjo2LjIyNjA1MzYzOTg0Njc0MywiZmxpcHBlZCI6ZmFsc2UsInZvbHVtZVVuZGVybGF5Ijp0cnVlLCJhZGoiOnRydWUsImNyb3NzaGFpciI6dHJ1ZSwiY2hhcnRUeXBlIjoibGluZSIsImV4dGVuZGVkIjpmYWxzZSwibWFya2V0U2Vzc2lvbnMiOnt9LCJhZ2dyZWdhdGlvblR5cGUiOiJvaGxjIiwiY2hhcnRTY2FsZSI6ImxpbmVhciIsInBhbmVscyI6eyJjaGFydCI6eyJwZXJjZW50IjoxLCJkaXNwbGF5IjoiQkNJLlNOIiwiY2hhcnROYW1lIjoiY2hhcnQiLCJpbmRleCI6MCwieUF4aXMiOnsibmFtZSI6ImNoYXJ0IiwicG9zaXRpb24iOm51bGx9LCJ5YXhpc0xIUyI6W10sInlheGlzUkhTIjpbImNoYXJ0Iiwi4oCMdm9sIHVuZHLigIwiXX19LCJzZXRTcGFuIjp7Im11bHRpcGxpZXIiOjUsImJhc2UiOiJ5ZWFyIiwicGVyaW9kaWNpdHkiOnsicGVyaW9kIjoxLCJpbnRlcnZhbCI6IndlZWsifSwibWFpbnRhaW5QZXJpb2RpY2l0eSI6dHJ1ZSwiZm9yY2VMb2FkIjp0cnVlfSwibGluZVdpZHRoIjoyLCJzdHJpcGVkQmFja2dyb3VuZCI6dHJ1ZSwiZXZlbnRzIjpmYWxzZSwiY29sb3IiOiIjMDA4MWYyIiwic3RyaXBlZEJhY2tncm91ZCI6dHJ1ZSwiZXZlbnRNYXAiOnsiY29ycG9yYXRlIjp7ImRpdnMiOnRydWUsInNwbGl0cyI6ZmFsc2V9LCJzaWdEZXYiOnt9fSwiY3VzdG9tUmFuZ2UiOm51bGwsInN5bWJvbHMiOlt7InN5bWJvbCI6IkJDSS5TTiIsInN5bWJvbE9iamVjdCI6eyJzeW1ib2wiOiJCQ0kuU04iLCJxdW90ZVR5cGUiOiJFUVVJVFkiLCJleGNoYW5nZVRpbWVab25lIjoiQW1lcmljYS9TYW50aWFnbyJ9LCJwZXJpb2RpY2l0eSI6MSwiaW50ZXJ2YWwiOiJ3ZWVrIiwidGltZVVuaXQiOm51bGwsInNldFNwYW4iOnsibXVsdGlwbGllciI6NSwiYmFzZSI6InllYXIiLCJwZXJpb2RpY2l0eSI6eyJwZXJpb2QiOjEsImludGVydmFsIjoid2VlayJ9LCJtYWludGFpblBlcmlvZGljaXR5Ijp0cnVlLCJmb3JjZUxvYWQiOnRydWV9fV0sInN0dWRpZXMiOnsi4oCMdm9sIHVuZHLigIwiOnsidHlwZSI6InZvbCB1bmRyIiwiaW5wdXRzIjp7ImlkIjoi4oCMdm9sIHVuZHLigIwiLCJkaXNwbGF5Ijoi4oCMdm9sIHVuZHLigIwifSwib3V0cHV0cyI6eyJVcCBWb2x1bWUiOiIjMDBiMDYxIiwiRG93biBWb2x1bWUiOiIjZmYzMzNhIn0sInBhbmVsIjoiY2hhcnQiLCJwYXJhbWV0ZXJzIjp7IndpZHRoRmFjdG9yIjowLjQ1LCJjaGFydE5hbWUiOiJjaGFydCIsInBhbmVsTmFtZSI6ImNoYXJ0In19fX0-" target="_blank">  
+                          BCI.SN
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+    
+                          </a>
+                        </td>
+                        <td>
+                          <a href="https://es.investing.com/equities/bci-(sn)-advanced-chart" target="_blank">
+                          bci-(sn)
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+    
+                          </a>
+                        </td>
+                        <td>
+                          Banco Estado
+                        </td>
+                        <td>
+                          <a href="" target="_blank">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="" target="_blank">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="<?php echo home_url();?>/Content/?ID=41">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+                            <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+                          </a>
+                        </td>
+                      </tr>
+                      <!-- --------------------------------------------------------------------------------------------- -->
+
+                                            
+
+                              
+     <?php
+
+//https://stackoverflow.com/questions/30531600/wordpress-how-do-i-get-all-posts-from-wp-query-in-the-search-results
+
+//https://developer.wordpress.org/reference/classes/wp_query/
+
+//https://nicolamustone.blog/2017/03/01/post-meta-get-value/
+
+//https://developer.wordpress.org/reference/functions/get_post_meta/
+
+//https://developer.wordpress.org/reference/functions/wp_get_post_categories/
+
+//https://wordpress.stackexchange.com/questions/247859/how-to-get-category-id-of-current-post
+
+
+
+
+global $wpdb;
+    /*pass your search string here example like this ( 's'=>'test' ) */
+    /*
+   $args=array('s'=>'bci','order'=> 'DESC', 'posts_per_page'=>get_option('posts_per_page'));
+
+   $query=new WP_Query($args);
+
+    if( $query->have_posts()): 
+
+    while( $query->have_posts()): $query->the_post();
+
+     {
+     echo $post->post_title;
+     echo $post->post_content;
+     }
+
+    endwhile; 
+    else:
+    endif;
+    */
+
+
+
+   $args=array('s'=>'bci','order'=> 'DESC', 'posts_per_page'=>get_option('posts_per_page'));
+
+   $query=new WP_Query($args);
+
+    if( $query->have_posts()): 
+
+    while( $query->have_posts()): $query->the_post();
+
+     {
+     ?>
+
+
+
+
+                      <!-- --------------------------------------------------------------------------------------------- -->
+                      <tr>
+                        <td>
+                          BANCO DE CREDITO E INVERSIONES
+                        </td>
+                        <td>
+                          <a href="">
+                          <?php echo $post->post_title; ?>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="" target="_blank">  
+                          
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+    
+                          </a>
+                        </td>
+                        <td>
+                          <a href="" target="_blank">
+                          
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+    
+                          </a>
+                        </td>
+                        <td>
+                          
+                        </td>
+                        <td>
+                          <a href="" target="_blank">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="" target="_blank">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+                            <i class="now-ui-icons business_globe"></i>
+                          </a>
+                        </td>
+                        <td>
+                          <a href="">
+                            <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+                          </a>
+                        </td>
+                      </tr>
+                      <!-- --------------------------------------------------------------------------------------------- -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
+<?php
+     }
+
+    endwhile; 
+    else:
+    endif;
+
+
+
+
+
+  ?>
+                      
+
+                      
+
+                      
+
+
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+
+            </div>
+          </div>
+    
+        </div>
+
+        <div class="row">
+
+
+        </div>          
+    </div>
+
+ <?php get_template_part( 'system_footer' ); ?>
